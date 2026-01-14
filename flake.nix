74437958be62c18a5bc196a47fa4696e2c786cdf
Klee Wiese <wiesenklee@riseup.net>
2026-01-14 11:19:31 +0100

    chore: change to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
 
